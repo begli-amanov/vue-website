ef810e287f9a595492c247e37c7d11634299f335
begli.amanov <begli.amanov@outlook.com>
2024-10-26 08:30:20 +0200

    Update vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 			resolvers: [PrimeVueResolver()],
 		}),
 	],
-	base: '/vue-website/',
+	base: './',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
